f3ae5c886f40fa1750abc07dfbebd3426460b339
Luis José López Delgado <luisjoselopezd@gmail.com>
2023-08-04 15:12:22 -0400

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "regenerator-runtime/runtime";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+const supportedChainIds = [1, 4];
+const connectors = {
+    injected: {},
+};
+const activeChain = "goerli";
+
+function App({ Component, pageProps }: AppProps) {
+    return (
+        <ThirdwebWeb3Provider
+            activeChain={activeChain}
+            supportedChainIds={supportedChainIds}
+            connectors={connectors}
+        >
+            <Component {...pageProps} />
+        </ThirdwebWeb3Provider>
+    );
 }
+
+export default App;
